009fc78757038a3244c7edc70d4394a57b31ffd2
Ruan Pinheiro Lima <ruanpinheirolima2003@gmail.com>
2023-11-21 16:15:59 -0300

    Update JS
    adicionado scroll suave aos links internos do site

--- script.js
+++ script.js
@@ -44,3 +44,21 @@ function initAccordion() {
     }
 }
 initAccordion()
+
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+
+function scrollSection(event){
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute('href')
+    const section = document.querySelector(href)
+    section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+    })
+}
+
+
+linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollSection)
+})
