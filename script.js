44e001898b50d2b202e790e33bca43a791ccdb75
Ruan Pinheiro Lima <ruanpinheirolima2003@gmail.com>
2023-11-21 15:15:16 -0300

    Create Accordion List

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li')
-const tabContent = document.querySelectorAll('.js-tabcontent section')
+function initTabNav() {
+
+    const tabMenu = document.querySelectorAll('.js-tabmenu li')
+    const tabContent = document.querySelectorAll('.js-tabcontent section')
 
 
-if (tabContent.length && tabMenu.length) {
-    
-    tabContent[0].classList.add('ativo')
+    if (tabContent.length && tabMenu.length) {
 
-    function activeTab(index) {
+        tabContent[0].classList.add('ativo')
 
-        tabContent.forEach((i) => {
-            i.classList.remove('ativo')
+        function activeTab(index) {
+
+            tabContent.forEach((i) => {
+                i.classList.remove('ativo')
+            })
+
+            tabContent[index].classList.add('ativo')
+        }
+
+        tabMenu.forEach((img, index) => {
+            img.addEventListener('click', () => {
+                activeTab(index)
+            })
         })
-
-        tabContent[index].classList.add('ativo')
     }
-
-    tabMenu.forEach((img, index) => {
-        img.addEventListener('click', () => {
-            activeTab(index)
-        })
-    })
 }
+initTabNav()
+
+function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt')
+
+    if (accordionList.length) {
+        accordionList[0].classList.add('ativo')
+        accordionList[0].nextElementSibling.classList.add('ativo')
+
+        function activeAccordion() {
+            this.classList.toggle('ativo')
+            this.nextElementSibling.classList.toggle('ativo')
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion)
+        })
+    }
+}
+initAccordion()
